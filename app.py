076b41ab1662828873413054f25f0ef51292d657
Leo-smart-consolidated <leo_da_niel@outlook.com>
2026-04-09 20:10:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -336,7 +336,14 @@ with tab1:
     grnzrooted26 = grouping(nzrooted26)
     
     # Incluir imagen como encabezado
-    st.image("header.png", use_container_width=True)
+    
+    from PIL import Image
+    
+    img = Image.open("header.png")
+
+    import os
+    st.write("Archivos en raíz:", os.listdir("."))
+    st.image(img, use_container_width=True
     
     col1, col2, col3, col4 = st.columns(4)
     with col1:
